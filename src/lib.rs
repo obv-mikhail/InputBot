8edbe1a4154459b54c2ccfe9d0262d2db2ced2ae
obv-mikhail <obv.mikhail@gmail.com>
2017-06-20 06:28:42 -0400

    move hotkeys to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,46 @@
-#[macro_use] 
+#[macro_use]
 extern crate lazy_static;
+
+use std::sync::{Arc, Mutex};
+use std::collections::hash_map::HashMap;
+use std::ops::{FnOnce, FnMut};
+use std::marker::Send;
+
+#[derive(Debug, Eq, PartialEq, Hash)]
+pub enum Hotkey {
+    KeybdHotkey(KeybdHotkeyType, VKCode), 
+    MouseHotkey(MouseHotkeyType)
+}
+
+#[derive(Debug, Eq, PartialEq, Hash)]
+pub enum KeybdHotkeyType {
+    Press,
+    Release
+}
+
+#[derive(Debug, Eq, PartialEq, Hash)]
+pub enum MouseHotkeyType {
+    PressLeft, 
+    ReleaseLeft, 
+    PressRight, 
+    ReleaseRight, 
+    PressMiddle, 
+    ReleaseMiddle, 
+    Move,
+    Scroll
+}
+
+lazy_static! {
+    static ref HOTKEYS: Arc<Mutex<HashMap<Hotkey, Box<FnMut() + Send + 'static>>>> = Arc::new(Mutex::new(HashMap::<Hotkey, Box<FnMut() + Send + 'static>>::new()));
+}
+
+pub fn register<F>(hotkey: Hotkey, callback: F) where for<'r> F: FnOnce() + 'static + Send + FnMut() {
+    HOTKEYS.lock().unwrap().insert(hotkey, Box::new(callback));
+}
+
+pub fn unregister(hotkey: Hotkey) {
+    HOTKEYS.lock().unwrap().remove(&hotkey);
+}
 
 #[cfg(target_os = "windows")]
 pub mod windows;
--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -33,50 +33,11 @@ extern crate user32;
 use self::winapi::*;
 use self::user32::*;
 use std::mem::{transmute_copy, transmute, size_of, uninitialized};
-use std::sync::{Arc, Mutex};
-use std::collections::hash_map::HashMap;
-use std::ops::{FnOnce, FnMut};
-use std::marker::Send;
+use HOTKEYS;
 
 pub mod vk;
 
 pub type VKCode = u8;
-
-#[derive(Debug, Eq, PartialEq, Hash)]
-pub enum Hotkey {
-    KeybdHotkey(KeybdHotkeyType, VKCode), 
-    MouseHotkey(MouseHotkeyType)
-}
-
-#[derive(Debug, Eq, PartialEq, Hash)]
-pub enum KeybdHotkeyType {
-    Press,
-    Release
-}
-
-#[derive(Debug, Eq, PartialEq, Hash)]
-pub enum MouseHotkeyType {
-    PressLeft, 
-    ReleaseLeft, 
-    PressRight, 
-    ReleaseRight, 
-    PressMiddle, 
-    ReleaseMiddle, 
-    Move,
-    Scroll
-}
-
-lazy_static! {
-    static ref HOTKEYS: Arc<Mutex<HashMap<Hotkey, Box<FnMut() + Send + 'static>>>> = Arc::new(Mutex::new(HashMap::<Hotkey, Box<FnMut() + Send + 'static>>::new()));
-}
-
-pub fn register<F>(hotkey: Hotkey, callback: F) where for<'r> F: FnOnce() + 'static + Send + FnMut() {
-    HOTKEYS.lock().unwrap().insert(hotkey, Box::new(callback));
-}
-
-pub fn unregister(hotkey: Hotkey) {
-    HOTKEYS.lock().unwrap().remove(&hotkey);
-}
 
 static mut KEYBD_HHOOK: HHOOK = 0 as HHOOK;
 static mut MOUSE_HHOOK: HHOOK = 0 as HHOOK;
