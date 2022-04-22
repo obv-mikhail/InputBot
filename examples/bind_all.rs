e1380823c12cb638e54d2140bde449647de81843
Rob Wagner <rob@sombia.com>
2022-04-22 01:06:37 -0400

    Break examples/test.rs into several tighter, more focused examples.
    Additionally, added more comments and explanations.

---
+++ examples/bind_all.rs
@@ -0,0 +1,21 @@
+use inputbot::KeybdKey;
+
+/// This example demonstrates binding all of the keyboard keys to a simple callback.
+/// The callback prints the key that was pressed, if it is found.
+
+fn main() {
+    // This is not strictly neccesary, but by calling this function, you can avoid a 'startup delay'
+    // when executing your first callback.
+    inputbot::init_device();
+
+    // Bind all keys to a common callback event.
+    KeybdKey::bind_all(|event| {
+        match inputbot::from_keybd_key(event) {
+            Some(c) => println!("{c}"),
+            None => println!("Unregistered Key"),
+        };
+    });
+
+    // Call this to start listening for bound inputs.
+    inputbot::handle_input_events();
+}
---
+++ examples/key_sequences.rs
@@ -0,0 +1,23 @@
+use inputbot::{KeySequence, KeybdKey::*};
+
+/// This example demonstrates sending sequences of key presses / characters via a KeySequence.
+/// This can be used, for example, to create a macro which types a specific string.
+
+fn main() {
+    // This is not strictly neccesary, but by calling this function, you can avoid a 'startup delay'
+    // when executing your first callback. This can impact key sequences by cutting off the first
+    // few characters.
+    inputbot::init_device();
+
+    // Bind our Backquote key (`, ~) to a callback that types out the string "Hello, world!".
+    // You must remember to call the `.send()` method on the KeySequence after creating it.
+    // You could explicitly define the KeySequence ahead of time and send it later like so:
+    //      let seq: KeySequence = KeySequence("Hello, world!");
+    //      seq.send();
+    BackquoteKey.bind(|| {
+        KeySequence("Hello, world!").send();
+    });
+
+    // Call this to start listening for bound inputs.
+    inputbot::handle_input_events();
+}
